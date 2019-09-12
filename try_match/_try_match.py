97dde9336f1a068d58250b5993423c629de95c51
ZhengYu, Xu <zen-xu@outlook.com>
2019-09-12 15:21:40 +0800

    add python3 support

--- try_match/_try_match.py
+++ try_match/_try_match.py
@@ -1,53 +1,57 @@
-from abc import ABCMeta
 
-import collections, inspect
+try:
+    from collections.abc import Container
+except ImportError:
+    from collections import Container
 
+import inspect
+import sys
 
-class Meta(ABCMeta):
-    def __subclasscheck__(cls, subclass):
-        value = getattr(subclass, "value", None)
-        case = getattr(cls, "case", None)
-        if not (value and case):
-            return False
-
-        if inspect.isclass(case):
-            return isinstance(value, case)
-        elif callable(case):
-            return case(value)
-        elif not isinstance(case, collections.Iterable):
-            return case == value
-        elif isinstance(case, collections.Iterable):
-            return value in case
-        else:
-            return False
+class _Base(Exception):
+    pass
 
 
-class DefaultCase(Exception):
-    __metaclass__ = ABCMeta
-
-
-class ProxyException(Exception):
-    __metaclass__ = ABCMeta
+DefaultCase = Exception
 
 
 def match(value):
-    class _Match(Exception):
-        __metaclass__ = ABCMeta
-    _Match.value = value
-    ProxyException.register(_Match)
-    DefaultCase.register(_Match)
+    class _Match(_Base):
+        pass
 
-    raise _Match
+    _Match.value = value
+    raise _Match("match nothing")
 
 
 def Case(case):
-    class _Case(Exception):
-        __metaclass__ = Meta
+    """
+    case can be value, class, range, lambda
+    """
 
-    if callable(case):
-        _Case.case = staticmethod(case)
-    else:
-        _Case.case = case
-    _Case.register(ProxyException)
+    class _Case(_Base):
+        pass
+
+    match_cls, _, _ = sys.exc_info()
+    value = getattr(match_cls, "value", None)
+
+    if value:
+        matched = False
+
+        if inspect.isclass(case) and isinstance(value, case):
+            matched = True
+
+        elif callable(case) and not inspect.isclass(case):
+            matched = case(value)
+
+        elif not isinstance(case, Container):
+            matched = value == case
+
+        elif isinstance(case, Container) and isinstance(case, str):
+            matched = value == case
+
+        elif isinstance(case, Container) and not isinstance(case, str):
+            matched = value in case
+
+        if matched:
+            match_cls.__bases__ = (_Case,)
 
     return _Case
